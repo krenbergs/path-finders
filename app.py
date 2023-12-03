f68ce2da567f67c44d2d28336054dd7c9c32fa9e
krenbergs <133226693+krenbergs@users.noreply.github.com>
2023-12-03 23:17:04 +0100

    Added environment
    Added a function to generate a 3d environment using perlin noise

--- app.py
+++ app.py
@@ -1,9 +1,51 @@
 from flask import Flask
 from flask import render_template
+import random
+import numpy as np
+import noise
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    coordinates = [(1, 2, 3), (4, 5, 6), (7, 8, 9)]
-    return render_template("index.html", coordinates=coordinates)
+    environment = generate_3d_environment(size=20, seed=random.random()*1000)
+    return render_template("index.html", coordinates=environment)
+
+
+def generate_3d_maze(n):
+    # Initialize list of wall coordinates
+    walls = []
+    
+    # Randomly assign walls
+    for x in range(n):
+        for y in range(n):
+            for z in range(n):
+                if random.choice([0, 1]) == 1:
+                    walls.append([x, y, z])
+    
+    return walls
+
+def generate_3d_environment(size, scale=0.1, octaves=6, persistence=0.5, lacunarity=2.0, height_threshold=0.0, seed=0):
+    # Create a 3D grid of coordinates in the range of 0 to n
+    x = np.linspace(0, size, size)
+    y = np.linspace(0, size, size)
+    z = np.linspace(0, size, size)
+    coords = np.array(np.meshgrid(x, y, z)).T.reshape(-1,3)
+
+    # Generate Perlin noise
+    perlin_noise = np.zeros((size, size, size))
+    for i in range(size):
+        for j in range(size):
+            for k in range(size):
+                perlin_noise[i][j][k] = noise.pnoise3(i*scale + seed, j*scale + seed, k*scale + seed, octaves=octaves, persistence=persistence, lacunarity=lacunarity, repeatx=size, repeaty=size, repeatz=size)
+
+    # Generate environment
+    environment = []
+    for i in range(size):
+        for j in range(size):
+            for k in range(size):
+                # Only include points that are above the height threshold
+                if perlin_noise[i][j][k] > height_threshold:
+                    environment.append([i, j, k])
+
+    return environment
